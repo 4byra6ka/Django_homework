14fce3d4a1db978bd0d26f028bd6c5bf8e67c595
gau <gau@1jz.ru>
2023-05-27 09:57:11 +0300

    Refactoring Task 6 homework 20.1

--- catalog/management/commands/utls.py
+++ catalog/management/commands/utls.py
@@ -12,8 +12,17 @@ class Command(BaseCommand):
             json_data = json.load(rfile)
             for data in json_data:
                 if data['model'] == 'catalog.category':
-                    category_for_create.append(Category(**data))
-                elif data['model'] == 'catalog.product':
-                    product_for_create.append(Product(**data))
-        Category.objects.bulk_create(category_for_create)
-        Product.objects.bulk_create(product_for_create)
+                    category_for_create.append(Category(**data['fields']))
+            Category.objects.bulk_create(category_for_create)
+            for data in json_data:
+                if data['model'] == 'catalog.product':
+                    # Category.objects.get(pk=1)
+                    product_for_create.append(Product(
+                        name=data['fields']['name'],
+                        description=data['fields']['description'],
+                        category=Category.objects.get(pk=data['fields']['category']),
+                        price=data['fields']['price'],
+                        create_date=data['fields']['create_date'],
+                        last_change_date=data['fields']['last_change_date'],
+                    ))
+            Product.objects.bulk_create(product_for_create)
